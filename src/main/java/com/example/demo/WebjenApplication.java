32c0aac81c7cec410590dfdbc49e832bf3dbe105
IrinDaisy1207 <irindaisy216@gmail.com>
2023-08-29 09:20:13 +0530

    first commit

---
+++ src/main/java/com/example/demo/WebjenApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebjenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebjenApplication.class, args);
+	}
+
+}
